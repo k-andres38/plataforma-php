64fd5a2eb5ab8e98dfe20c100c89112f6c3f9015
Carlos Navarro <kandres38@gmail.com>
2022-12-17 18:01:10 -0500

    cuadre del perfil

--- public/plataforma.php
+++ public/plataforma.php
@@ -20,6 +20,8 @@ if(!isset($id)){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="../css/perfil.css">
+
     <title>Inicio</title>
 </head>
 
@@ -47,9 +49,15 @@ if(!isset($id)){
 
                 <ul class=" navbar-nav mb-2 mb-lg-0">
                 <li class="nav-item dropdown   d-flex">
-                        <a class="nav-link dropdown-toggle p-0" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                
+                        <a class="nav-link dropdown-toggle p-0 d-flex flex-column justify-content-center  align-items-end" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <div class="index2">
+                            <img class="perfil" src="../imagenes/logo.webp" alt="">
+                        </div>
                             <?= $nombre ?>
+                           
                         </a>
+                        
                         <ul class="dropdown-menu p-0">
                             <li><a class="dropdown-item" href="#">Action</a></li>
                             <li><a class="dropdown-item" href="#">Another action</a></li>
